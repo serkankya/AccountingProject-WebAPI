0396ad78a781cf27a38a42c400177c5718ae6951
Serkan Kaya <serkankaya0721@gmail.com>
2025-08-12 11:53:52 +0300

    add unitTest for updateRole & deleteRole

---
+++ Project.UnitTest/Features/RoleFeatures/Commands/DeleteRoleCommandUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using Project.Application.Features.AppFeatures.RoleFeatures.Commands.DeleteRole;
+using Project.Application.Services.AppServices;
+using Project.Domain.MainEntities.Identity;
+using Shouldly;
+
+namespace Project.UnitTest.Features.RoleFeatures.Commands
+{
+	public sealed class DeleteRoleCommandUnitTest
+	{
+		readonly Mock<IRoleService> _roleService;
+
+		public DeleteRoleCommandUnitTest()
+		{
+			_roleService = new();
+		}
+
+		[Fact]
+		public async Task RoleShouldNotBeNull()
+		{
+			_roleService.Setup(x => x.GetById(It.IsAny<string>())).ReturnsAsync(new AppRole());
+		}
+
+		[Fact]
+		public async Task DeleteRoleCommandResponseShouldNotBeNull()
+		{
+			DeleteRoleCommand request = new(
+				Id: "Test Id");
+
+			_roleService.Setup(x => x.GetById(It.IsAny<string>())).ReturnsAsync(new AppRole());
+
+			var handler = new DeleteRoleCommandHandler(_roleService.Object);
+
+			DeleteRoleCommandResponse response = await handler.Handle(request, default);
+			response.ShouldNotBeNull();
+			response.Message.ShouldNotBeEmpty();
+		}
+	}
+}
---
+++ Project.UnitTest/Features/RoleFeatures/Commands/UpdateRoleCommandUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using Project.Application.Features.AppFeatures.RoleFeatures.Commands.UpdateRole;
+using Project.Application.Services.AppServices;
+using Project.Domain.MainEntities.Identity;
+using Shouldly;
+
+namespace Project.UnitTest.Features.RoleFeatures.Commands
+{
+	public sealed class UpdateRoleCommandUnitTest
+	{
+		readonly Mock<IRoleService> _roleService;
+
+		public UpdateRoleCommandUnitTest()
+		{
+			_roleService = new();
+		}
+
+		[Fact]
+		public async Task RoleShouldNotBeNull()
+		{
+			_roleService.Setup(x => x.GetById(It.IsAny<string>())).ReturnsAsync(new AppRole());
+		}
+
+		[Fact]
+		public async Task RoleCodeShouldBeNull()
+		{
+			AppRole role = (await _roleService.Object.GetByCode("Test Code")!);
+			role.ShouldBeNull();
+		}
+
+		[Fact]
+		public async Task UpdateRoleCommandResponseShouldNotBeNull()
+		{
+			UpdateRoleCommand request = new(
+				Id: "Test Id",
+				Code: "Test Code",
+				Name: "Test Name");
+
+			_roleService.Setup(x => x.GetById(It.IsAny<string>())).ReturnsAsync(new AppRole());
+
+			var handler = new UpdateRoleCommandHandler(_roleService.Object);
+
+			UpdateRoleCommandResponse response = await handler.Handle(request, default);
+			response.ShouldNotBeNull();
+			response.Message.ShouldNotBeEmpty();
+		}
+	}
+}
